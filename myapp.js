8538a6dd7e801b5cce69c4b936ffd54ae96b26e2
andziaoo7 <a.kalamacka@gmail.com>
2015-09-03 15:19:31 +0200

    koszyk

--- myapp.js
+++ myapp.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('store', ['ngRoute']);
+	var app = angular.module('store', []);
 	app.controller('StoreController', function(){
 		this.products = categories;
 	});
@@ -110,209 +110,132 @@
 
 	}];
 
-	app.controller('RightsideController', function(){
+	app.controller('RightsideController', function($scope, ProductService){
 		this.products = products;
+		$scope.products = ProductService.getAll();
+
 	});
-	var products = [
+
+	app.controller('ProductController', function($scope, ProductService){
+		//var id = product.id; określic var id
+		$scope.product = ProductService.getProductById(id);
+	});
+
+//app.service odwołuje też produkty do rightsidecontrollera (index2) i będzie też odwoływał je do index3
+	app.service('ProductService', function(){
+		var products = [
 	
-	{
-		id: 1,
-		name: 'Automat na karmę na microchip',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7116460P.jpg'
-	},
-	{
-		id: 2,
-		name: 'Pojemnnik na jedzenie',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7108756P.jpg'
+			{
+				id: 1,
+				name: 'Automat na karmę na microchip',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7116460P.jpg',
+				price: 10.00
+			},
+			{
+				id: 2,
+				name: 'Pojemnnik na jedzenie',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7108756P.jpg',
+				price: 10.00
+			},
+			{
+				id: 3,
+				name: 'Miski ceramiczne',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7114740P.jpg',
+				price: 10.00
+			},
+			{
+				id: 4,
+				name: 'Kuwety',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/37039P.jpg',
+				price: 10.00
+			},
+			{
+				id: 5,
+				name: 'Smycze',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/P3659.jpg',
+				price: 10.00
+			},
+			{
+				id: 6,
+				name: 'Szelki',
+				link: 'index3.html',
+				img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/28539P.jpg',
+				price: 10.00
+			}
+		];
 
-	},
-	{
-		id: 3,
-		name: 'Miski ceramiczne',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/7114740P.jpg'
 
-	},
-	{
-		id: 4,
-		name: 'Kuwety',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/37039P.jpg'
-
-	},
-	{
-		id: 5,
-		name: 'Smycze',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/P3659.jpg'
-
-	},
-	{
-		id: 6,
-		name: 'Szelki',
-		link: 'index3.html',
-		img: 'http://media.petsathome.com/wcsstore/pah-cas01//173/28539P.jpg'
-
-	}];
-
-	app.directive('recommendation', function(){
 		return {
-			restrict: 'E',
-			templateUrl: 'recommendation.html'
-		};
-	});
-	
-	
 
-	app.config(['$routeProvider', function($routeProvider) {
+			getAll: function() {
+				return products;
+			},
+
+			getProductById: function(id) {
+				var product = products.filter(function(prod){
+					return prod.id === id;
+				});
+
+				if (condition) {
+					// Zwróć pierwszy objekt z tablicy
+				} else {
+					// Przekieruj na główną stronę - redirectTo
+				};
+			}
+
+		};
+
+	});
+
+
+	/*app.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			
 			//route for home page
 			.when('/', {
 				templateUrl: 'index.html',
-				/*controller: 'StoreController'*/
+				controller: 'mainController'
 			})
 
 			//route for product category - automat
 			.when('/automat', {
 				templateUrl: 'automat.html',
-				controller: 'automatController'
+				controller: 'mainController'
 			})
 
 			.when('/pojemnik', {
 				templateUrl: 'pojemnik.html',
-				controller: 'boxController'
+				controller: 'mainController'
 			})
 
 			.when('/miski', {
 				templateUrl: 'miski.html',
-				controller:  'bowlController'
+				controller:  'mainController'
 			})
 
 			.when('/kuwety', {
 				templateUrl: 'kuwety.html',
-				controller:  'litterController'
+				controller:  'mainController'
 			})
 
 			.when('/smycze', {
 				templateUrl: 'smycze.html',
-				controller: 'leashController'
+				controller: 'mainController'
 			})
 
 			.when('/szelki', {
 				templateUrl: 'szelki.html',
-				controller: 'bodyController'
+				controller: 'mainController'
 			});
 	}]);
 
 
-	app.controller('StoreController', function($scope) {
-		$scope.message = 'Everyone come and see how good I look!';
-	});
+;*/
 
-	app.controller('automatController', function($scope) {
-		$scope.message = 'automat page';
-	});
-
-	app.controller('boxController', function($scope) {
-		$scope.message = 'box page';
-	});
-
-	app.controller('bowlController', function($scope) {
-		$scope.message = 'bowl page';
-	});
-
-	app.controller('litterController', function($scope) {
-		$scope.message = 'litter page';
-	});
-
-	app.controller('leashController', function($scope) {
-		$scope.message = 'leash page';
-	});
-
-	app.controller('bodyController', function($scope) {
-		$scope.message = 'body page';
-	});
-
-
-
-/*
-	app.controller('mainController', function($scope, CartService) {
-		$scope.message = 'bla bla bla';
-
-		var product = {
-			id: 1,
-			price: 22.99,
-			url: 'http://image.jpg',
-			title: 'aasdasdasd'
-		};
-
-		// $scope.addToCart = CartService.addItem(product); dodawanie koszyka 
-		$scope.addToCart = function(product) {
-
-			var item = {
-				id: product.id,
-				price: product.price
-			};
-
-			CartService.addItem(item);
-		};
-	});
-
-
-	app.service('CartService', function(){
-		var items = [];
-
-		return {
-
-			addItem: function(product) {
-
-				var inCart = items.filter(function(item){
-					return item.id === product.id;
-				});
-
-				if ( !!inCart.length ) {
-					var obj = inCart[0];
-					obj.qty += 1;
-
-					var index;
-
-					// 
-
-					items[index].qty += 1;
-
-					// var newItems = items.filter(function(item){
-					// 	return item.id !== product.id;
-					// });
-					// newItems.push(obj);
-					// TODO - set qty + 1
-
-				} else {
-					// inCart => [];
-					items.push(product);
-				}
-			},
-
-			removeItem: function(id) {
-
-			},
-
-			getTotalQty: function() {
-
-			},
-
-			getTotalCost: function() {
-				var total = 0;
-				// forEach
-				return 123.89;
-
-			}
-
-		};
-
-	});*/
-
-
+	
 })();
